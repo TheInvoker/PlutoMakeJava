15981236a286d68d30f5b49cecdf89dedd6e691c
TheInvoker <09dsouzb@utsc.utoronto.ca>
2015-08-19 21:07:59 -0400

    updated master.js structure

--- PlutoMakeJava/bin/master.js
+++ PlutoMakeJava/bin/master.js
@@ -1,74 +1,82 @@
 [
-	{
-		"grid" : "grid/multigrid.png",
-		"warp" : "template/card1/warp2.png",
-		"template" : "template/card1/business-card.jpg",
-		"result" : "template/card1/result/",
-		"mapping" : "template/card1/mapping.txt",
-		"metadata" : "template/card1/metadata.txt",
-		"transformer" : "bilinear",
-		"filter" : "darken",
-		"mask" : "",
-		"active" : true,
-		"extra" : [
-		],
-		"x" : 488,
-		"y" : 289,
-		"w" : 155,
-		"h" : 155
-	},
-	{
-		"grid" : "grid/multigrid.png",
-		"warp" : "template/bump1/warp3.png",
-		"template" : "template/bump1/purse.png",
-		"result" : "template/bump1/result/",
-		"mapping" : "template/bump1/mapping.txt",
-		"metadata" : "template/bump1/metadata.txt",
-		"transformer" : "bilinear",
-		"filter" : "none",
-		"mask" : "",
-		"active" : true,
-		"extra" : [
-		],
-		"x" : 600,
-		"y" : 600,
-		"w" : 400,
-		"h" : 400
-	},
-	{
-		"grid" : "grid/plaingrid.png",
-		"warp" : "template/coaster1/warp.png",
-		"template" : "template/coaster1/coaster1.png",
-		"result" : "template/coaster1/result/",
-		"mapping" : "template/coaster1/mapping.txt",
-		"metadata" : "template/coaster1/metadata.txt",
-		"transformer" : "bilinear",
-		"filter" : "darken",
-		"mask" : "template/coaster1/mask.png",
-		"active" : true,
-		"extra" : [
-		],
-		"x" : 655,
-		"y" : 230,
-		"w" : 1000,
-		"h" : 1000
-	},
-	{
-		"grid" : "grid/multigrid.png",
-		"warp" : "template/coffeecup1/warp.png",
-		"template" : "template/coffeecup1/coffee_cup.png",
-		"result" : "template/coffeecup1/result/",
-		"mapping" : "template/coffeecup1/mapping.txt",
-		"metadata" : "template/coffeecup1/metadata.txt",
-		"transformer" : "1.5orderpoly",
-		"filter" : "darken",
-		"mask" : "",
-		"active" : true,
-		"extra" : [
-		],
-		"x" : 605,
-		"y" : 391,
-		"w" : 1000,
-		"h" : 1000
-	}
+	[
+		{
+			"grid" : "grid/multigrid.png",
+			"warp" : "template/card1/warp2.png",
+			"template" : "template/card1/business-card.jpg",
+			"result" : "template/card1/result/",
+			"mapping" : "template/card1/mapping.txt",
+			"metadata" : "template/card1/metadata.txt",
+			"transformer" : "bilinear",
+			"filter" : "darken",
+			"mask" : "",
+			"active" : true,
+			"extra" : [
+			],
+			"x" : 488,
+			"y" : 289,
+			"w" : 155,
+			"h" : 155
+		}
+	],
+	[
+		{
+			"grid" : "grid/multigrid.png",
+			"warp" : "template/bump1/warp3.png",
+			"template" : "template/bump1/purse.png",
+			"result" : "template/bump1/result/",
+			"mapping" : "template/bump1/mapping.txt",
+			"metadata" : "template/bump1/metadata.txt",
+			"transformer" : "bilinear",
+			"filter" : "none",
+			"mask" : "",
+			"active" : true,
+			"extra" : [
+			],
+			"x" : 600,
+			"y" : 600,
+			"w" : 400,
+			"h" : 400
+		}
+	],
+	[
+		{
+			"grid" : "grid/plaingrid.png",
+			"warp" : "template/coaster1/warp.png",
+			"template" : "template/coaster1/coaster1.png",
+			"result" : "template/coaster1/result/",
+			"mapping" : "template/coaster1/mapping.txt",
+			"metadata" : "template/coaster1/metadata.txt",
+			"transformer" : "bilinear",
+			"filter" : "darken",
+			"mask" : "template/coaster1/mask.png",
+			"active" : true,
+			"extra" : [
+			],
+			"x" : 655,
+			"y" : 230,
+			"w" : 1000,
+			"h" : 1000
+		}
+	],
+	[
+		{
+			"grid" : "grid/multigrid.png",
+			"warp" : "template/coffeecup1/warp.png",
+			"template" : "template/coffeecup1/coffee_cup.png",
+			"result" : "template/coffeecup1/result/",
+			"mapping" : "template/coffeecup1/mapping.txt",
+			"metadata" : "template/coffeecup1/metadata.txt",
+			"transformer" : "1.5orderpoly",
+			"filter" : "darken",
+			"mask" : "",
+			"active" : true,
+			"extra" : [
+			],
+			"x" : 605,
+			"y" : 391,
+			"w" : 1000,
+			"h" : 1000
+		}
+	]
 ]
