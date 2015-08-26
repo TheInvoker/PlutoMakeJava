a7b13667a9881590cae719492a99a3ae3d7f11c1
TheInvoker <09dsouzb@utsc.utoronto.ca>
2015-08-25 22:39:04 -0400

    update

--- PlutoMakeJava/src/MyJSON.java
+++ PlutoMakeJava/src/MyJSON.java
@@ -1,40 +1,17 @@
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.IntBuffer;
-import java.nio.channels.FileChannel;
 import javax.imageio.ImageIO;
 
 public final class MyJSON {
 	
     public final static BufferedImage GenerateWarpedLogo(BufferedImage logoImage, String maskPath, String mappingPath) throws IOException
     {
-    	// reads the binary data into in[]
-    	FileInputStream stream = new FileInputStream(mappingPath);
-        File f = new File(mappingPath);
-        
-        FileChannel inChannel = stream.getChannel();
-        ByteBuffer buffer = inChannel.map(FileChannel.MapMode.READ_ONLY, 0, inChannel.size());
-        
-        inChannel.close();
-        stream.close();
-        
-        int size = (int) f.length() >> 2;  // divide by 4
-        int[] xy = new int[2], result = new int[size];
-
-        buffer.order(ByteOrder.LITTLE_ENDIAN);
-        IntBuffer intBuffer = buffer.asIntBuffer();
-        intBuffer.get(result);
-
-        
-        // read width, height
-        cantor_pair_reverse(result[0], xy);
-        int i, newIndex, x=0, y=0, width = xy[0];
-        
+        BufferedImage mappingImage = ImageIO.read(new File(mappingPath));
+        int x=0, y=0, width=mappingImage.getWidth(), height=mappingImage.getHeight();
+        int[] xy = new int[2];
+        Color color;
 
         // new warped logo
         BufferedImage flag = new BufferedImage(logoImage.getWidth(), logoImage.getHeight(), BufferedImage.TYPE_INT_ARGB);
@@ -47,54 +24,51 @@ public final class MyJSON {
         }
         
 
-        for(i=1; i<size; i+=1) {
-        	if (result[i] < 0) {
-                newIndex = width * y + x - result[i];
-                y = newIndex / width;
-                x = newIndex - (width * y);
-        	} else {
-            	cantor_pair_reverse(result[i], xy);
+        
+        for(x=0; x<width; x+=1) {
+        	for(y=0; y<height; y+=1) {
 
-                
-                
-                
-                
-               
-                Color pixel = Exporter.getColor(logoImage.getRGB(xy[0], xy[1]));
+        		color = Exporter.getColor(mappingImage.getRGB(x, y));
+        		
+        		if (color.getAlpha() != 0) {
 
-                int newA = pixel.getAlpha();
-                if (maskImage == null) {
-                	flag.setRGB(x, y, (new Color(pixel.getRed(), pixel.getGreen(), pixel.getBlue(), newA)).getRGB());
-                } else {
-                    int maskA = Exporter.getColor(maskImage.getRGB(x, y)).getAlpha();
-                    if (maskA != 0)
-                    {
-                        newA *= maskA / 255;
-                        flag.setRGB(x, y, (new Color(pixel.getRed(), pixel.getGreen(), pixel.getBlue(), newA)).getRGB());
-                    }
-                }
+	            	cantor_pair_reverse(getint(color.getRed(), color.getGreen(), color.getBlue()), xy);
 
-                
-                
-                
-                
-                
-                x += 1;
-                if (x >= width)
-                {
-                    x = 0;
-                    y += 1;
-                }
-        	}
-        }  
+	                Color pixel = Exporter.getColor(logoImage.getRGB(xy[0], xy[1]));
+	
+	                int newA = pixel.getAlpha();
+	                if (maskImage == null) {
+	                	flag.setRGB(x, y, (new Color(pixel.getRed(), pixel.getGreen(), pixel.getBlue(), newA)).getRGB());
+	                } else {
+	                    int maskA = Exporter.getColor(maskImage.getRGB(x, y)).getAlpha();
+	                    if (maskA != 0)
+	                    {
+	                        newA *= maskA / 255;
+	                        flag.setRGB(x, y, (new Color(pixel.getRed(), pixel.getGreen(), pixel.getBlue(), newA)).getRGB());
+	                    }
+	                }
+	        	}
+	    	}
+	    } 
         
         if (maskImage != null)
         {
         	maskImage.flush();
         }
+        
+        mappingImage.flush();
 
     	return flag;
     }
+    
+    private final static int getint(int r, int g, int b)
+    {
+        int rl = (r << 16) & 0xFF0000;
+        int gl = (g << 8) & 0x00FF00;
+        int bl = b & 0x0000FF;
+        return rl | gl | bl;
+    }
+    
     
     /**
      * Return the source integers from a cantor pair integer.
